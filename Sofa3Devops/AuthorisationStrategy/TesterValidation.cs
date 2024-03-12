64d77441df3da6875b420ad182a384733efbda72
Xin Wang <xx.wang@student.avans.nl>
2024-03-12 18:59:10 +0100

    Renamed to tester validation

---
+++ Sofa3Devops/AuthorisationStrategy/TesterValidation.cs
@@ -0,0 +1,21 @@
+﻿using Sofa3Devops.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sofa3Devops.AuthorisationStrategy
+{
+    public class TesterValidation : IAuthValidationBehavior
+    {
+        public bool HasPermission(Member member)
+        {
+            if (member.GetType() == typeof(Tester))
+            {
+                return true;
+            }
+            throw new UnauthorizedAccessException($"Unauthorized action: Users with {member} role are not allowed to perform this action. Only testers are allowed.");
+        }
+    }
+}
