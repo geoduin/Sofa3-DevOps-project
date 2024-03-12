9b98c7445d768778f8cf2ff8ab9842bbcc53fe32
Xin Wang <xx.wang@student.avans.nl>
2024-03-12 18:54:50 +0100

    Push files

---
+++ Sofa3Devops/AuthorisationStrategy/IAuthValidationBehavior.cs
@@ -0,0 +1,14 @@
+﻿using Sofa3Devops.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sofa3Devops.AuthorisationStrategy
+{
+    public interface IAuthValidationBehavior
+    {
+        public bool HasPermission(Member member);
+    }
+}
