d4b043c7509ef5118e2b638078a23cdf8bbe97f7
Xin Wang <xx.wang@student.avans.nl>
2024-03-12 18:55:28 +0100

    Renamed to all allowed

---
+++ Sofa3Devops/AuthorisationStrategy/AllAllowedValidation.cs
@@ -0,0 +1,17 @@
+﻿using Sofa3Devops.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sofa3Devops.AuthorisationStrategy
+{
+    public class AllAllowedValidation : IAuthValidationBehavior
+    {
+        public bool HasPermission(Member member)
+        {
+            return true;
+        }
+    }
+}
