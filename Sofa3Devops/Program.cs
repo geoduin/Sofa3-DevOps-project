8403fae4c6cc526803c5430fed54a93eeaad0075
Xin Wang <xx.wang@student.avans.nl>
2024-03-14 20:40:39 +0100

    Removed dummy code

--- Sofa3Devops/Program.cs
+++ Sofa3Devops/Program.cs
@@ -14,48 +14,6 @@ namespace Sofa3Devops
     {
         static void Main(string[] args)
         {
-            ProductOwner productOwner = new ProductOwner("Po", "PO@example.com", "PO");
-            CompositeComponent BaseComposite = new CompositeComponent("Release pipeline 1");
-            Pipeline CICDPipeline = new Pipeline(BaseComposite);
-            ReleaseSprint Sprint = new ReleaseSprint(DateTime.Now, DateTime.Now.AddDays(3), "Release pipeline", CICDPipeline);
-
-
-            // Arrange
-            BuildStage buildStage = new BuildStage("Build project");
-            Command command = new Command("Import packages", "npm Install");
-            Command buildProject = new Command("Import packages", "npm build");
-
-            TestStage testStage = new TestStage("Test project");
-            Command testCommand = new Command("Perform unit test", "npm unit-test");
-            Command integrationTest = new Command("Perform integration test", "npm integration-test");
-
-            AnalyzeStage analysisStage = new AnalyzeStage("Perform code analysis");
-            Command analysisCommand = new Command("Perform Sonar scan", "npm sonarcloud-analysis");
-
-            DeploymentStage deployStage = new DeploymentStage("Deploy code");
-            Command deployCommand = new Command("Perform deployment to Railway", "upload deploy-railway");
-
-            // Commands for build
-            buildStage.AddComponent(command);
-            buildStage.AddComponent(buildProject);
-            // Tests
-            testStage.AddComponent(testCommand);
-            testStage.AddComponent(integrationTest);
-
-            // Analysis
-            analysisStage.AddComponent(analysisCommand);
-
-            // Deployment
-            deployStage.AddComponent(deployCommand);
-
-            // Add build stage to pipeline
-            BaseComposite.AddComponent(buildStage);
-            BaseComposite.AddComponent(testStage);
-            BaseComposite.AddComponent(analysisStage);
-            BaseComposite.AddComponent(deployStage);
-
-            // Act
-            var result = Sprint.StartReleasePipeline(productOwner);
         }
     }
 }
