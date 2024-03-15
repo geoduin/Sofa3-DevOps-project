7bd6544034c4504a377a2619576b33826eb3250c
Xin Wang <xx.wang@student.avans.nl>
2024-03-15 18:21:27 +0100

    UC-15 Done

---
+++ Sofa3Devops/Domain/SprintSummary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sofa3Devops.Domain
+{
+    public class SprintSummary
+    {
+        private readonly string Title;
+        private readonly List<string> Notes;
+        private readonly DateTime UploadDate;
+
+        public SprintSummary(string title, List<string> notes) { 
+            Title = title;
+            Notes = notes;
+            UploadDate = DateTime.Now;
+        }
+    }
+}
